0bf6abc3af16cc4a025a013777584ee959e8ab82
poorna2152 <58025190+poorna2152@users.noreply.github.com>
2022-07-18 20:11:54 +0530

    Refactor diagnostics

--- src/diagnostics.cpp
+++ src/diagnostics.cpp
@@ -8,10 +8,10 @@
 #include "parser.h"
 #include "utils.h"
 
-struct TokenDetails
+struct NodeDetails
 {
     std::string end_col;
-    std::string token;
+    std::string node;
     std::string start_line;
     std::string start_col;
     std::string end_line;
@@ -34,19 +34,19 @@ std::string escape_sequence(std::string type) {
     return type;
 }
 
-void write_token(Parser *parser, std::string file, TSNode cur, std::map<std::string, std::vector<TokenDetails>>& nodes)
+void write_node(std::string file, TSNode cur, std::map<std::string, std::vector<NodeDetails>>& nodes)
 {
-    TokenDetails details;
+    NodeDetails details;
     TSPoint start_point = ts_node_start_point(cur);
     TSPoint end_point = ts_node_end_point(cur);
     details.start_col = std::to_string(start_point.column + 1);
     details.start_line = std::to_string(start_point.row + 1);
     details.end_col = std::to_string(end_point.column + 1);
     details.end_line = std::to_string(end_point.row + 1);
-    details.token = escape_sequence(ts_node_type(cur));
+    details.node = escape_sequence(ts_node_type(cur));
     if (nodes.count(file) > 0)
     {
-        TokenDetails last = nodes[file].back();
+        NodeDetails last = nodes[file].back();
         if (last.start_col == details.start_col && last.start_line == details.start_line && last.end_col == details.end_col && last.end_line == details.end_line) {
             nodes[file].pop_back();
         }
@@ -54,15 +54,47 @@ void write_token(Parser *parser, std::string file, TSNode cur, std::map<std::str
     }
     else
     {
-        std::vector<TokenDetails> tokens(1, details);
-        nodes[file] = tokens;
+        std::vector<NodeDetails> nodes_init(1, details);
+        nodes[file] = nodes_init;
     }
+}
+
+void write_to_json_file(const std::map<std::string, std::vector<NodeDetails>>& file_nodes_map) {
+    std::ofstream output_file("out.json");
+    output_file << "{"
+                << "\n";
+    for (const std::pair<std::string, std::vector<NodeDetails>> &file_nodes : file_nodes_map)
+    {
+        output_file << "\t\"" + file_nodes.first + "\": [ \n";
+        std::vector<NodeDetails> nodes = file_nodes.second;
+        for (NodeDetails& node : nodes)
+        {
+            std::string obj = "\t\t{\n"
+                              "\t\t\t\"start_col\": " + node.start_col + ",\n"
+                              "\t\t\t\"start_line\": " + node.start_line + ",\n"
+                              "\t\t\t\"end_col\": " + node.end_col + ",\n"
+                              "\t\t\t\"end_line\": " + node.end_line + ",\n"
+                              "\t\t\t\"node\": \"" + node.node + "\"\n"
+                                  "\t\t}";
+            if (&node == &nodes.back())
+            {
+                output_file << obj << "\n";
+            }
+            else
+            {
+                output_file << obj << ",\n";
+            }
+        }
+        output_file << "\t]"
+                    << "\n";
+    }
+    output_file << "}"
+                << "\n";
 }
 
 void diagnose(Parser *parser, const std::vector<std::string> &files)
 {
-    std::map<std::string, std::vector<std::string>> json;
-    std::map<std::string, std::vector<TokenDetails>> nodes;
+    std::map<std::string, std::vector<NodeDetails>> file_nodes_map;
     for (std::string file : files)
     {
         TSTree *tree = parser->parse_file(file);
@@ -73,7 +105,7 @@ void diagnose(Parser *parser, const std::vector<std::string> &files)
             if (ts_tree_cursor_goto_first_child(&cursor) || ts_tree_cursor_goto_next_sibling(&cursor))
             {
                 TSNode cur = ts_tree_cursor_current_node(&cursor);
-                write_token(parser, file, cur, nodes);
+                write_node(file, cur, file_nodes_map);
                 continue;
             }
             bool had_sibling = true;
@@ -88,7 +120,7 @@ void diagnose(Parser *parser, const std::vector<std::string> &files)
             }
             TSNode cur = ts_tree_cursor_current_node(&cursor);
             if (had_sibling) {
-                write_token(parser, file, cur, nodes);
+                write_node(file, cur, file_nodes_map);
             }
             if (ts_node_is_null(ts_node_parent(cur)))
             {
@@ -96,36 +128,7 @@ void diagnose(Parser *parser, const std::vector<std::string> &files)
             }
         }
     }
-    std::ofstream output_file("out.json");
-    output_file << "{"
-                << "\n";
-    for (const auto &myPair : nodes)
-    {
-        std::cout << myPair.first << "\n";
-        output_file << "\t\"" + myPair.first + "\": [ \n";
-        for (size_t i = 0; i < myPair.second.size(); i++)
-        {
-            std::string obj = "\t\t{\n"
-                              "\t\t\t\"start_col\": " + myPair.second[i].start_col + ",\n"
-                              "\t\t\t\"start_line\": " + myPair.second[i].start_line + ",\n"
-                              "\t\t\t\"end_col\": " + myPair.second[i].end_col + ",\n"
-                              "\t\t\t\"end_line\": " + myPair.second[i].end_line + ",\n"
-                              "\t\t\t\"token\": \"" + myPair.second[i].token + "\"\n"
-                                  "\t\t}";
-            if (i != myPair.second.size() - 1)
-            {
-                output_file << obj << ",\n";
-            }
-            else
-            {
-                output_file << obj << "\n";
-            }
-        }
-        output_file << "\t]"
-                    << "\n";
-    }
-    output_file << "}"
-                << "\n";
+    write_to_json_file(file_nodes_map);
 }
 
 
