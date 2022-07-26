eed99235ef48d2b0fa3ad310bff5de21b80824c8
poorna2152 <58025190+poorna2152@users.noreply.github.com>
2022-07-26 19:27:44 +0530

    Update counting

--- src/count.cpp
+++ src/count.cpp
@@ -4,51 +4,94 @@
 #include <cstring>
 #include "parser.h"
 #include "utils.h"
+#include <memory>
 extern "C"
 {
 #include <tree_sitter/api.h>
 }
 
+// update a boolean if only the cur node is a function call node
+void check_call_node(const char *type, bool &is_parent_call) {
+    // types of function nodes: call, call_expression, binary_expression(eg: std::cout << "hello")
+    if ((!strcmp("call", type) || !strcmp("call_expression", type) || !strcmp("binary_expression", type)))
+    {
+        is_parent_call = true;
+    }
+}
+
+// check if the cur node is a function definition node
+void check_func_def_node(const char *type, bool &is_parent_func_def) {
+    if ((!strcmp("function_definition", type)))
+    {
+        is_parent_func_def = true;
+    }
+}
+
+// name of the function called is met in the node which is met before meeting the arguement list node
+void check_call_identifier_end(const std::string identifier, const std::string &feature, const char *type, int &count, bool &is_parent_call) {
+    if ((!strcmp("argument_list", type) || !strcmp("<<", type)) && is_parent_call)
+    {
+        is_parent_call = false;
+        if (identifier == feature) {
+            count += 1;
+        }
+    }
+}
+
+// increment count if type of the node matches the counting node type
+bool is_node_type_match(const std::string &feature, const char *type) {
+    if (!strncmp(feature.c_str(), type, strlen(type) > feature.length() ? strlen(type) : feature.length()))
+    {
+        return true;
+    }
+    return false;
+}
+
 int count_feature(Parser *parser, const Countable &countable, const std::string &feature, const std::vector<std::string> &files)
 {
     int count = 0;
-    bool is_matching_parent = false;
+    bool is_parent_func_def = false;
+    bool is_parent_call = false;
     for (std::string file : files)
     {
         TSTree *tree = parser->parse_file(file);
         TSNode root_node = ts_tree_root_node(tree);
+        std::string identifier;
         TSTreeCursor cursor = ts_tree_cursor_new(root_node);
         while (true)
         {
             TSNode cur = ts_tree_cursor_current_node(&cursor);
             const char *type = ts_node_type(cur);
             // check if cur node is a identifier; used when counting identifiers, functions or calls
-            if (!strcmp("identifier", type) && (countable == IDENTIFIER || is_matching_parent))
+            if (!strcmp("identifier", type) || !strcmp("field_identifier", type))
             {
                 int start = ts_node_start_byte(cur);
                 int end = ts_node_end_byte(cur);
                 // retrieve identifier name
-                if (parser->get_identifier(start, end) == feature)
+                identifier = parser->get_identifier(start, end);
+                if (identifier == feature)
                 {
-                    count += 1;
+                    if (is_parent_func_def && countable == FUNCTION)
+                    {
+                        count += 1;
+                    }
+                    else if (countable == IDENTIFIER)
+                    {
+                        count += 1;
+                    }
                 }
-                is_matching_parent = false;
+                is_parent_func_def = false;
             }
-            // cur nodes type matches requested feature
-            if (!strncmp(feature.c_str(), type, strlen(type) > feature.length() ? strlen(type) : feature.length()))
-            {
+            // node checks
+            if (countable == NODE && is_node_type_match(feature, type)) {
                 count += 1;
             }
-            if (!is_matching_parent) {
-                // call node of the tree can have three possible types checked here
-                if ((!strcmp("call", type) || !strcmp("call_expression", type) || !strcmp("binary_expression", type)) && countable == CALL)
-                {
-                    is_matching_parent = true;
-                }
-                else if ((!strcmp("function_definition", type)) && countable == FUNCTION)
-                {
-                    is_matching_parent = true;
-                }
+            else if (countable == FUNCTION) {
+                check_func_def_node(type, is_parent_func_def);
+            }
+            else if (countable == CALL) {
+                check_call_node(type, is_parent_call);
+                check_call_identifier_end(identifier, feature, type, count, is_parent_call);
             }
             if (ts_tree_cursor_goto_first_child(&cursor) || ts_tree_cursor_goto_next_sibling(&cursor))
             {
