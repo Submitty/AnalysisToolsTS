901d6239b9770f641b5963864504051e9282ff82
Matthew Peveler <matt@popsql.com>
2022-09-17 12:41:14 -0400

    Rework test suite to use pytest

---
+++ tests/test_integration.py
@@ -0,0 +1,33 @@
+from pathlib import Path
+import subprocess
+import json
+import pytest
+from typing import Dict, List, Tuple
+
+this_dir = Path(__file__).parent.resolve()
+fixtures_dir = this_dir / 'fixtures'
+exe_path = this_dir / '..' / 'build' / 'submitty_count_ts'
+
+def execute_command(file: str, command: str) -> int:
+    args = [exe_path]
+    args.extend((f"-l {file.split('.')[1]} {command} {str(this_dir / 'fixtures' / file)}").split(" "))
+    result = subprocess.run(args, capture_output=True)
+    output = result.stdout.decode().strip("\n")
+    if (not output.isnumeric()):
+        raise Exception("Result is not numeric")
+    return int(output)
+
+
+with (this_dir / 'fixtures' / 'commands.json').open() as file:
+    commands: Dict[str, List[Tuple[str, int]]] = json.load(file)
+
+testcases = []
+
+file: str
+for file in commands.keys():
+    for test in commands[file]:
+        testcases.append([file, test[0], test[1]])
+
+@pytest.mark.parametrize("file, command, expected_output", testcases)
+def test_integration(file, command, expected_output):
+    assert expected_output == execute_command(file, command)
