6d29b9e63442bba6f6719241ac597d32681f48db
Matthew Peveler <matt.peveler@gmail.com>
2023-02-02 08:52:42 -0500

    [Testing:Developer] Rework test suite to use pytest (#11)
    Co-authored-by: William Allen <16820599+williamjallen@users.noreply.github.com>

---
+++ tests/test_integration.py
@@ -0,0 +1,34 @@
+from pathlib import Path
+import subprocess
+import json
+import pytest
+from typing import Dict, List, Tuple
+
+this_dir = Path(__file__).parent.resolve()
+fixtures_dir = this_dir / 'fixtures'
+exe_path = this_dir / '..' / 'build' / 'submitty_count_ts'
+
+def execute_command(file: str, countable: str, feature: str) -> int:
+    args = [str(exe_path), '-l', file.split('.')[-1], countable, feature, str(this_dir / 'fixtures' / file)]
+    print(args)
+    result = subprocess.run(args, capture_output=True)
+    output = result.stdout.decode().strip("\n")
+    print(output)
+    if not output.isnumeric():
+        raise Exception("Result is not numeric")
+    return int(output)
+
+
+with (this_dir / 'fixtures' / 'commands.json').open() as file:
+    commands: Dict[str, List[Tuple[str, int]]] = json.load(file)
+
+testcases = []
+
+file: str
+for file in commands:
+    for test in commands[file]:
+        testcases.append([file, test[0], test[1], test[2]])
+
+@pytest.mark.parametrize("file, countable, feature, expected_output", testcases)
+def test_integration(file, countable, feature, expected_output):
+    assert expected_output == execute_command(file, countable, feature)
