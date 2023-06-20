876a684b791bdcc4d905590e01ecbd0f8dbd8602
IDzyre <reapernsgaming@gmail.com>
2023-06-20 10:51:56 -0800

    testing fix

--- versions.sh
+++ versions.sh
@@ -9,12 +9,12 @@
 
 # TREE-SITTER REPOS
 # Python -- Merge pull request #193 from tree-sitter/tausbn/... -- Feb 28, 23
-export tree-sitter-python_hash=62827156d01c74dc1538266344e788da74536b8a
+export tree-sitter-python-hash=62827156d01c74dc1538266344e788da74536b8a
 # Tree-Sitter - Avoid unused value warning from array_pop -- Jun 14, 23
-export tree-sitter_hash=d0029a15273e526925a764033e9b7f18f96a7ce5
+export tree-sitter-hash=d0029a15273e526925a764033e9b7f18f96a7ce5
 # cpp -- Merge pull request #209 from amaanq/rewrite-it-in-c -- Jun 18, 23
-export tree-sitter-cpp_hash=2e29f23abcb5f92ddffb22de8b292f09ed78db01
+export tree-sitter-cpp-hash=2e29f23abcb5f92ddffb22de8b292f09ed78db01
 # C -- Merge pull request #141 from JasonBrownDeveloper/offsetof -- Jun 13, 23
-export tree-sitter-c_hash=f1b2a0b2102ff001a4ed9a26cd9a9405d09c590c
+export tree-sitter-c-hash=f1b2a0b2102ff001a4ed9a26cd9a9405d09c590c
 # Java -- Merge pull request #144 from stelf/patch-1 -- April 19, 23
-export tree-sitter-java_hash=c194ee5e6ede5f26cf4799feead4a8f165dcf14d
+export tree-sitter-java-hash=c194ee5e6ede5f26cf4799feead4a8f165dcf14d
